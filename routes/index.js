ddaae6162b95f7a38ae0a3d536e7f78436cf0661
Albertojuanse <albertojuansebastian@gmail.com>
2016-04-27 17:19:11 +0200

    Tema3: Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controler');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/questions', quizController.question);
+router.get('/check', quizController.check);
 
 module.exports = router;
